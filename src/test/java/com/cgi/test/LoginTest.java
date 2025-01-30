f644be1dd5de192472ca311da802a4fd707db78b
Admin <Admin@host.docker.internal>
2025-01-30 16:02:34 +0530

    Updated Valid and Invalid Login Test

--- src/test/java/com/cgi/test/LoginTest.java
+++ src/test/java/com/cgi/test/LoginTest.java
@@ -1,6 +1,7 @@
 package com.cgi.test;
 
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import com.cgi.base.AutomationWrapper;
@@ -9,16 +10,20 @@ public class LoginTest extends AutomationWrapper {
 	@Test
 	public void validLoginTest() {
 		driver.findElement(By.name("username")).sendKeys("Admin");
-		// enter password as admin123
-		// click on login
-		// Assert the QuickLaunch Text
+		driver.findElement(By.name("password")).sendKeys("admin123");
+		driver.findElement(By.xpath("//button[contains(normalize-space(),'Login')]")).click();
+
+		String actualValue = driver.findElement(By.xpath("//p[contains(normalize-space(),'Quick')]")).getText();
+		Assert.assertEquals(actualValue, "Quick Launch");
 	}
 
 	@Test
 	public void invalidLoginTest() {
 		driver.findElement(By.name("username")).sendKeys("john");
-		// enter password as john123
-		// click on login
+		driver.findElement(By.name("password")).sendKeys("john123");
+		driver.findElement(By.xpath("//button[contains(normalize-space(),'Login')]")).click();
 		// Assert the error - Invalid credentials
+		String actualError = driver.findElement(By.xpath("//p[contains(normalize-space(),'Invalid')]")).getText();
+		Assert.assertEquals(actualError, "Invalid credentials");
 	}
 }
